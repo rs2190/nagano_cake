01fa73b6d18fb7d9674d78e1419bd300a044abd3
RyuSuzuki <122139610+rs2190@users.noreply.github.com>
2023-02-26 14:36:22 +0000

    [Add] 2023/02/26 orderdetailモデルに製作ステータスのenum実装

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,6 @@
 class OrderDetail < ApplicationRecord
+
+  # [Add] 2023/02/26 orderdetailモデルに製作ステータスのenum実装
+  enum making_status: { cannot_start: 0, waiting_for_payment: 1, in_production: 2, completion_of_production: 3 }
+
 end
