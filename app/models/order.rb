d92246a83c8aa6b1ee41296ceb3c94b3bc406d75
RyuSuzuki <122139610+rs2190@users.noreply.github.com>
2023-02-26 14:00:27 +0000

    [Add] 2023/02/26 [Add] 2023/02/26 orderモデルに注文ステータスのenum実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,8 @@ class Order < ApplicationRecord
   # [Add] 2023/02/26 orderモデルに支払方法のenum実装
   enum payment_method: { credit_card: 0, transfer: 1 }
 
+  # [Add] 2023/02/26 orderモデルに注文ステータスのenum実装
+  enum order_status: { waiting_for_payment: 0, deposit_confirmation: 1, in_production: 2, preparing_to_ship: 3, sent: 4 }
+
 
 end
