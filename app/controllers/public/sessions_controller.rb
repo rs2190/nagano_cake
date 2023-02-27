b638599258e268d78669ae8b58ec8611e4917c12
RyuSuzuki <122139610+rs2190@users.noreply.github.com>
2023-02-27 10:49:11 +0000

    [Add] 2023/02/27 devise管理者側,顧客側のview,controller実装

---
+++ app/controllers/public/sessions_controller.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class Public::SessionsController < Devise::SessionsController
+
+  # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+
+
+  protected
+
+  # 退会しているかを判断するメソッド
+  def customer_state
+      ## 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+
+    ## 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted
+      ##  trueだった場合、退会しているのでサインアップ画面に遷移する
+      redirect_to new_customer_registration_path
+    end
+  
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # # 【顧客用】
+  # devise_for :customers
+  # # 【管理者用】
+  # devise_for :admins
+
+  # [Add] 2023/02/27 devise管理者側,顧客側のview,controller実装
   # 【顧客用】
-  devise_for :customers
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+
+  }
+
   # 【管理者用】
-  devise_for :admins
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 
 end
