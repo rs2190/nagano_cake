5f54300b5c62fa5bb78662b239b133c4e3a8cbb2
RyuSuzuki <122139610+rs2190@users.noreply.github.com>
2023-02-28 12:36:53 +0000

    [Add] 2023/02/28 admin/customers routing実装

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,8 @@ Rails.application.routes.draw do
     resources :items , only: [:index, :new, :create, :show, :edit, :update]
     # [Add] 2023/02/28 admin/genres routing実装
     resources :genres , only: [:index, :create, :edit, :update]
+     # [Add] 2023/02/28 admin/customers routing実装
+    resources :customers , only: [:index, :show, :edit, :update]
 
   end
 
