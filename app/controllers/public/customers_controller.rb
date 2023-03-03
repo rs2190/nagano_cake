79332444757120f8b9fb47da4ede62e2f99186df
RyuSuzuki <122139610+rs2190@users.noreply.github.com>
2023-03-03 11:22:29 +0000

    [Add] 2023/03/03 顧客のマイページ実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,13 @@
+# "[Add] 2023/03/03 顧客のマイページ実装"
 class Public::CustomersController < ApplicationController
+
+  before_action :authenticate_customer!, only: [:show]
+
+  # 顧客のマイページ
   def show
+
+    @customer = Customer.find(current_customer.id)
+
   end
 
   def edit
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,36 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # "[Add] 2023/03/03 顧客のマイページ実装"
+
+  # 姓 + 名
+  def name
+
+    last_name + first_name
+
+  end
+
+  # 姓 + "□" + 名 (□は、全角スペース)
+  def name_blank
+
+    last_name + "　" + first_name
+
+  end
+
+    # 姓カナ + 名カナ
+  def name_kana
+
+    last_name_kana + first_name_kana
+
+  end
+
+  # 姓 + "□" + 名 (□は、全角スペース)
+  def name_kana_blank
+
+    last_name_kana + "　" + first_name_kana
+
+  end
+
+
 end
