67490f8baee64ebe418db4767db1fc07792a3166
RyuSuzuki <122139610+rs2190@users.noreply.github.com>
2023-03-03 12:42:04 +0000

    [Add] 2023/03/03 顧客の登録情報編集画面実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,8 +1,8 @@
-# "[Add] 2023/03/03 顧客のマイページ実装"
 class Public::CustomersController < ApplicationController
 
   before_action :authenticate_customer!, only: [:show]
 
+  # [Add] 2023/03/03 顧客のマイページ実装
   # 顧客のマイページ
   def show
 
@@ -11,7 +11,12 @@ class Public::CustomersController < ApplicationController
 
   end
 
+  # [Add] 2023/03/03 顧客の登録情報編集画面実装
+  # 顧客の登録情報編集画面
   def edit
+
+    @customer = customer_find
+
   end
 
   def update
