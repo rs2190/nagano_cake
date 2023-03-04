6c782776d8b8a9f0b432d4030bbc922a3e051e3c
RyuSuzuki <122139610+rs2190@users.noreply.github.com>
2023-03-04 13:10:03 +0000

    [Add] 2023/03/04 配送先登録/一覧画面実装

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,5 +1,14 @@
 class Public::AddressesController < ApplicationController
+
+  # [Add] 2023/03/04 配送先登録/一覧画面実装
+  before_action :authenticate_customer!, only: [:index ,:edit]
+
+  # 配送先登録/一覧画面
   def index
+
+    @address = Address.new
+    @addresses = Address.where(customer_id: current_customer.id)
+
   end
 
   def edit
