9e3c1fadc0649414815eb5d87350b716291e1f96
RyuSuzuki <122139610+rs2190@users.noreply.github.com>
2023-03-07 09:57:47 +0000

    [Add] 2023/03/07 定数定義用のファイル実装

---
+++ config/initializers/constants.rb
@@ -0,0 +1,15 @@
+# [Add] 2023/03/07 定数定義用のファイル実装
+
+# 定数定義用のファイル作成
+module Constants
+
+  # 空文字
+  HALF_WIDTH_SPACE = ""
+
+  # 半角スペース
+  HALF_WIDTH_SPACE = " "
+
+  # 全角スペース
+  FULL_WIDTH_SPACE = "　"
+
+end
