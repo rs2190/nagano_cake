e7b179b42e7940162d4a584803ecbed3341a4caa
RyuSuzuki <122139610+rs2190@users.noreply.github.com>
2023-03-08 11:22:04 +0000

    [Add] 2023/03/08 商品新規登録画面実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,8 +1,18 @@
 class Admin::ItemsController < ApplicationController
+
+   # [Add] 2023/03/08 商品新規登録画面実装
+  before_action :authenticate_admin!, only: [:index, :new, :show, :edit]
+
+
   def index
   end
 
+  # [Add] 2023/03/08 商品新規登録画面実装
+  # 商品新規登録画面
   def new
+
+    @item = Item.new
+
   end
 
   def create
