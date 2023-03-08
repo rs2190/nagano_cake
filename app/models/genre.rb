a3e366022d8561cd389c538b2ebb91e6eeb08804
RyuSuzuki <122139610+rs2190@users.noreply.github.com>
2023-03-08 09:37:07 +0000

    [Add] 2023/03/08 genresとitemsの 1 : N のアソシエーション実装

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,7 @@
 class Genre < ApplicationRecord
+
+  # [Add] 2023/03/08 genresとitemsの 1 : N のアソシエーション実装
+  # 1 : N　の関連付け（アソシエーション: association）
+  has_many :items
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,8 @@ class Item < ApplicationRecord
   # 商品の画像をアップロードする
   has_one_attached :image
 
+  # [Add] 2023/03/08 genresとitemsの 1 : N のアソシエーション実装
+  # 1 : N　の関連付け（アソシエーション: association）
+  belongs_to :genre
+
 end
