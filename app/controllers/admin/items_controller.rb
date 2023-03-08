f6814f7b722bc6e9d34b11ffffb6250e712be2e2
RyuSuzuki <122139610+rs2190@users.noreply.github.com>
2023-03-08 11:48:49 +0000

    [Add] 2023/03/08 商品情報の新規登録実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -15,7 +15,24 @@ class Admin::ItemsController < ApplicationController
 
   end
 
+  # [Add] 2023/03/08 商品情報の新規登録実装
+  # 商品情報の新規登録
   def create
+
+    @item = Item.new(item_param)
+
+
+    if @item.save
+
+      redirect_to admin_items_path
+
+    else
+
+      render :new
+
+    end
+
+
   end
 
   def show
@@ -26,4 +43,14 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+
+  private
+
+  # ストロングパラメータ
+  def item_param
+
+    params.require(:item).permit(:image, :genre_id, :name, :introduction, :price ,:is_active)
+
+  end
+
 end
