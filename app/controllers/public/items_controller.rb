a94822ac5677b9bb21e28d12ea001324c1b32078
RyuSuzuki <122139610+rs2190@users.noreply.github.com>
2023-03-09 05:36:48 +0000

    [Add] 2023/03/09 商品一覧画面追加etc

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,12 +1,30 @@
 class Public::ItemsController < ApplicationController
 
+  # 商品一覧画面
   def index
 
     @genres = genre_all_asc
-    
+
+    # issue タグ検索未実装
+    if params[:id].present?
+
+      @items = Item.where(genre_id: params[:id]).order(id: "DESC")
+
+    else
+
+      @items = Item.all.order(id: "DESC")
+
+    end
+
 
   end
 
   def show
+
+    @genres = genre_all_asc
+    @item = item_find
+    @cart_item = CartItem.new
+
+
   end
 end
